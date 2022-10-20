0432f135bbde4c58ff8e90220f9dc5aefea91ee5
JustSimon01 <simon.ch@yandex.ru>
2022-10-20 17:06:15 +0300

    настроен swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.log('Входящий JS файл работает.');
+
+  // import Swiper JS
+  import Swiper, { Navigation, Pagination } from 'swiper';
+  Swiper.use([Navigation, Pagination]);
+  
+  const swiper = new Swiper('.persons__swiper', {
+    loop: false,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+  });
